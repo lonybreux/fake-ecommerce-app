5d3e57e70dae2576e4d97c1a0d14cb76dd6b6dde
Matias <carrillomatias121@gmail.com>
2026-06-12 15:57:54 -0500

    chore: add jwt secret

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -3,8 +3,10 @@ dotenv.config()
 
 if(!process.env.CLIENT_URL) throw new Error('CLIENT_URL no definido')
 if(!process.env.MONGODB_URI) throw new Error('MONGODB_URI no definido')
+if(!process.env.JWT_SECRET) throw new Error('JWT_SECRET no definido')
 
 export const PORT = process.env.PORT || 3000
 export const CLIENT_URL = process.env.CLIENT_URL
 export const MONGODB_URI = process.env.MONGODB_URI
+export const JWT_SECRET = process.env.JWT_SECRET
 
