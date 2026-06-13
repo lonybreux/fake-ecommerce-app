7f757c99a55c2d9524b11d76bf6193c76655d71a
Matias <carrillomatias121@gmail.com>
2026-06-12 21:54:52 -0500

    add comentario model

---
+++ backend/src/models/comentario.model.ts
@@ -0,0 +1,18 @@
+import { Document, model, Schema, Types } from "mongoose";
+
+export interface IComentario extends Document {
+    clienteId: Types.ObjectId
+    contenido: string
+}
+
+export interface ICreateComentario {
+    clienteId: Types.ObjectId
+    contenido: string
+}
+
+const comentarioSchema = new Schema<IComentario>({
+    clienteId: {type: Types.ObjectId, ref: 'Cliente', required: true},
+    contenido: {type: String, required: true}
+}, {timestamps: true})
+
+export const ComentarioModel = model<IComentario>('Comentario',comentarioSchema)
