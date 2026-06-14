67627cf86cee77042f2e116f27fc74e30d66f46d
Matias <carrillomatias121@gmail.com>
2026-06-14 13:34:49 -0500

    add pedido model

---
+++ backend/src/models/pedido.model.ts
@@ -0,0 +1,23 @@
+import { model, Schema, Types, type Document } from "mongoose";
+
+type EstadoPedido = 'pendiente' | 'enviado' | 'entregado' | 'cancelado'
+
+export interface IPedido extends Document {
+    clienteId: Types.ObjectId
+    estado: EstadoPedido
+    total: number
+}
+
+export interface ICreatePedidoDto {
+    clienteId: Types.ObjectId
+    estado: EstadoPedido
+    total: number
+}
+
+const pedidoSchema = new Schema<IPedido>({
+    clienteId: {type: Types.ObjectId, ref: 'Cliente', required: true},
+    estado: {type: String, enum: ['pendiente','enviado','entregado','cancelado'] , default: 'pendiente'},
+    total: {type: Number, required: true}
+}, {timestamps: true})
+
+export const PedidoModel = model<IPedido>('Pedido',pedidoSchema)
