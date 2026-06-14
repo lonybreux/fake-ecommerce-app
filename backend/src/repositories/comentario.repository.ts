5a09e8df38972e9051f49d68415e52588ca70662
Matias <carrillomatias121@gmail.com>
2026-06-13 21:39:30 -0500

    add comentario repository

---
+++ backend/src/repositories/comentario.repository.ts
@@ -0,0 +1,25 @@
+import { ComentarioModel, type IComentario } from "../models/comentario.model.js";
+import type IRepository from "./IRepository.js";
+
+
+export default class ComentarioRepository implements IRepository<IComentario> {
+    public async findAll(): Promise<IComentario[]> {
+        return await ComentarioModel.find()
+    }
+    public async findById(id: string): Promise<IComentario | null> {
+        return await ComentarioModel.findById(id)
+    }
+    public async findOne(entity: Partial<IComentario>): Promise<IComentario | null> {
+        return await ComentarioModel.findOne(entity)
+    }
+    public async create(entity: Partial<IComentario>): Promise<IComentario> {
+        return await ComentarioModel.create(entity)
+    }
+    public async update(id: string, entity: Partial<IComentario>): Promise<IComentario | null> {
+        return await ComentarioModel.findByIdAndUpdate(id,entity, {new: true, runValidators: true})
+    }
+    public async delete(id: string): Promise<void> {
+        await ComentarioModel.findByIdAndDelete(id)
+    }
+    
+}
