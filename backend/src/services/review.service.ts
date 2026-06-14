9cbd66388eee47bc4512275f43292a70b346a755
Matias <carrillomatias121@gmail.com>
2026-06-14 15:39:26 -0500

    feat: update product rating on review creation

--- backend/src/services/review.service.ts
+++ backend/src/services/review.service.ts
@@ -1,5 +1,5 @@
 import { Types } from "mongoose";
-import type { ICreateReview, IReview } from "../models/review.model.js";
+import type { ICreateReviewDto, IReview } from "../models/review.model.js";
 import type IRepository from "../repositories/IRepository.js";
 import type { IProducto } from "../models/producto.model.js";
 import ReviewRepository from "../repositories/review.repository.js";
@@ -26,8 +26,16 @@ export default class ReviewService {
         return await (this.reviewRepository as ReviewRepository).findMany({clienteId: new Types.ObjectId(id)})
     }
 
-    public async createReview(review: ICreateReview): Promise<IReview> {
-        return await this.reviewRepository.create(review)
+    public async createReview(review: ICreateReviewDto): Promise<IReview> {
+        const newReview = await this.reviewRepository.create(review)
+
+        const reviews = await (this.reviewRepository as ReviewRepository).findMany({productoId: review.productoId})
+
+        const rating = reviews.reduce((sumaRatings,r) => sumaRatings + r.rating, 0) / reviews.length
+
+        await this.productoRepository.update(review.productoId.toString(),{rating, totalReviews: reviews.length})
+
+        return newReview
     }
 
     public async deleteReview(id: string): Promise<void> {
