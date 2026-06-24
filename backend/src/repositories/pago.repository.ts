a9ae43613e72cffe82d870195c5b8fbab72d2e5f
Matias <carrillomatias121@gmail.com>
2026-06-24 17:21:19 -0500

    fix: add missing pago repository methods

--- backend/src/repositories/pago.repository.ts
+++ backend/src/repositories/pago.repository.ts
@@ -7,19 +7,19 @@ export default class PagoRepository implements IRepository<IPago> {
        return await PagoModel.find()
     }
     public async findById(id: string): Promise<IPago | null> {
-        
+        return await PagoModel.findById(id)
     }
     public async findOne(entity: Partial<IPago>): Promise<IPago | null> {
-        
+        return await PagoModel.findOne(entity)
     }
     public async create(entity: Partial<IPago>): Promise<IPago> {
-        
+        return await PagoModel.create(entity)
     }
     public async update(id: string, entity: Partial<IPago>): Promise<IPago | null> {
-        
+        return await PagoModel.findByIdAndUpdate(id,entity, {new: true, runValidators: true})
     }
     public async delete(id: string): Promise<void> {
-       
+       await PagoModel.findByIdAndDelete(id)
     }
     
 }
