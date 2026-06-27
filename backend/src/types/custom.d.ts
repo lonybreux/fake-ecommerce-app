347fa4e2bace5cef5d2a0ecf2f9c0d306be7baba
Matias <carrillomatias121@gmail.com>
2026-06-26 21:28:14 -0500

    feat: add rol field in jwt payload

--- backend/src/types/custom.d.ts
+++ backend/src/types/custom.d.ts
@@ -4,7 +4,7 @@ import type { JwtPayload } from "jsonwebtoken";
 declare global {
     namespace Express {
         interface Request {
-            user?: {_id: string, rol: 'cliente' | 'admin' } & JwtPayload
+            user?: JwtPayload & {_id: string, rol: 'cliente' | 'admin' }
         }
     }
 }
