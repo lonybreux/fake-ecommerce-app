95b336baf90b0986bedb02074952b809f260c87a
Matias <carrillomatias121@gmail.com>
2026-07-04 17:36:59 -0500

    feat: add status routes on auth routes

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -2,13 +2,14 @@ import { Router } from 'express'
 import AuthController from '../controllers/auth.controller.js'
 import ClienteService from '../services/cliente.service.js'
 import ClienteRepository from '../repositories/cliente.repository.js'
+import verificarToken from '../middlewares/auth.middleware.js'
 const router = Router()
 
 const clienteRepository = new ClienteRepository()
 const clienteService = new ClienteService(clienteRepository)
 const authController = new AuthController(clienteService)
 
-
+router.get('/status', verificarToken, authController.getStatus)
 router.post('/register', authController.register)
 router.post('/login',authController.login)
 
